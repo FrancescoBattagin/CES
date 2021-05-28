9d4050c97a266a0d9454ea0290824d23a2787e26
FrancescoBattagin <48253383+FrancescoBattagin@users.noreply.github.com>
2021-05-28 10:10:35 +0200

    Add files via upload

--- orchestrator.py
+++ orchestrator.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python2
+import grpc
+import os, sys
+from time import sleep
 import p4runtime_sh.shell as sh
+from scapy.all import *
+
+# Import P4Runtime lib from parent utils dir
+# Probably there's a better way of doing this.
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 'utils/p4runtime_lib'))
+import bmv2
+from error_utils import printGrpcError
+from switch import ShutdownAllSwitchConnections
+import helper
 
 def checkPolicies(packet):
     #read from db/txt file
@@ -11,11 +25,16 @@ def checkPolicies(packet):
             line = f.readline()
 
     found = False
+    if IP in packet:
+        ip_src=packet[IP].src
+        ip_dst=packet[IP].dst
+
     for policy in policies:
-        if device_id in policy and port in policy and desired_service in policy: #&& desired info are present
+        #TODO: check device_ip, port and destination of packet inside policies
+        if ip_src in policy and ip_dst in policy: #&& desired info are present
             found = True
-            #connection.stream_out_q.put(packet)
-            sendPacketToDataPlane()
+            #TODO: add entries in table  
+            addEntries(ip_src, ip_dst)          
             break
     if not found:
         #drop
@@ -23,41 +42,34 @@ def checkPolicies(packet):
         print("packet dropped")
 
 
-def sendPacketToDataPlane():
-    pktInRaw = send_pkt.getPacktInRaw()
-    req = p4runtime_pb2.StreamMessageRequest()
-    packet = p4runtime_pb2.PacketOut()
-    packet.payload = pktInRaw
-    
-    metadata = req.packet.metadata.add()
-    metadata.metadata_id=1
-    metadata.value = (1).to_bytes(2,'big')
-    metadata = req.packet.metadata.add()
-    metadata.metadata_id=2
-    metadata.value = (0).to_bytes(2,'big')
-    packet.metadata.append(metadata)
-    req.packet.CopyFrom(packet)
-
-    sh.client.stream_out_q.put(req)
-
-
-def receivePacketFromDataPlane():
-    send_pkt.sendPacket('send_to_cpu')
-    rep = sh.client.get_stream_packet('packet',timeout=2)
-    if rep is not None:
-        print('ingress port is',int.from_bytes(rep.packet.metadata[0].value,'big'))
+def addEntries(ip_src, ip_dst):
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="my_ingress.ipv4_lpm",
+        match_fields={
+            #FIX
+            "hdr.ipv4_t.srcAddr": ip_src,
+            "hdr.ipv4_t.dstAddr": ip_dst #ADD PORT?,
+        },
+        action_name="my_ingress.ipv4_forward",
+        action_params={}
+        )
+    ces.WriteTableEntry(table_entry)
+    print("Installed leader table entry rule on {}".format(ces.name))
 
 
 
-sh.send_to_cpu(
-    device_id=0,
-    grpc_addr='192.187.3.7:50001',
-    election_id=(0, 1), # (high, low)
-    #config=sh.FwdPipeConfig('p4-test.p4info.txt', 'p4-test.json')
-)
+p4info_file_path = "p4-test.p4info.txt"
+p4info_helper = helper.P4InfoHelper(p4info_file_path)
+ces = bmv2.Bmv2SwitchConnection(
+    name='ces',
+    address='192.187.3.7:50051',
+    device_id= 0,
+    proto_dump_file='logs/ces-p4runtime.txt')
+
+ces.MasterArbitrationUpdate()
 
 connection = sh.client
-
+#print(connection)
 while True:
     packet = None
     print("Waiting for receive something")
@@ -65,26 +77,7 @@ while True:
     
     if packet != None:
         print("Packet received!:" + str(packet))
-        #CHECK POLICIES
         checkPolicies(packet)
-
 
 #this code won't be accessible, but for the sake of completeness
 sh.teardown()
-
-
-
-#GitHub hints
-
-#TOADD to p4 program?
-# @controller_header("packet_in")
-# header packet_in_t {
-#     bit<9> ingress_port;
-#     bit<7> padding;
-# }
-
-# @controller_header("packet_out")
-# header packet_out_t {
-#     bit<9> egress_port;
-#     bit<7> padding;
-# }
