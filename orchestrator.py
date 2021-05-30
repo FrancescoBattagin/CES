1bc14c14e614ca13260ff616bef62619d5f73ae1
FrancescoBattagin <francesco.battagin@mail.polimi.it>
2021-05-30 10:45:05 +0200

    orchestrator fix

--- orchestrator.py
+++ orchestrator.py
@@ -3,7 +3,7 @@ import grpc
 import os, sys
 from time import sleep
 import p4runtime_sh.shell as sh
-#from scapy.all import *
+import socket
 
 # Import P4Runtime lib from parent utils dir
 # Probably there's a better way of doing this.
@@ -25,15 +25,14 @@ def checkPolicies(packet):
             line = f.readline()
 
     found = False
-    if IP in packet:
-        ip_src=packet[IP].src
-        ip_dst=packet[IP].dst
+    src_dst = ipv4_head()
+    ip_src= src_dst[0]
+    ip_dst= src_dst[1]
 
     for policy in policies:
         #TODO: check device_ip, port and destination of packet inside policies
         if ip_src in policy and ip_dst in policy: #&& desired info are present
             found = True
-            #TODO: add entries in table  
             addEntries(ip_src, ip_dst)          
             break
     if not found:
@@ -50,6 +49,26 @@ def addEntries(ip_src, ip_dst):
     print("Installed leader table entry rule on {}".format(ces.name))
 
 
+def ethernet_head(raw_data):
+    dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+    dest_mac = get_mac_addr(dest)
+    src_mac = get_mac_addr(src)
+    proto = socket.htons(prototype)
+    data = raw_data[14:]
+    return dest_mac, src_mac, proto, data 
+
+
+def ipv4_head(raw_data):
+    version_header_length = raw_data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', raw_data[:20])
+    data = raw_data[header_length:]
+    src_dst = [src, target]
+    return src_dst
+
+
+
 
 p4info_file_path = "p4-test.p4info.txt"
 p4info_helper = helper.P4InfoHelper(p4info_file_path)
@@ -60,13 +79,14 @@ ces = bmv2.Bmv2SwitchConnection(
     proto_dump_file='logs/ces-p4runtime.txt')
 
 ces.MasterArbitrationUpdate()
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
 connection = sh.client
 print(connection)
 while True:
     packet = None
     print("Waiting for receive something")
-    packet = connection.stream_in_q.get()
+    packet = s.recvfrom(65565) #max buff length
     
     if packet != None:
         print("Packet received!:" + str(packet))
