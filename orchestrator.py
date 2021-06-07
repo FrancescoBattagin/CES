63dc8a7b071638d11c238477eac706ea57e01e7a
FrancescoBattagin <francesco.battagin@mail.polimi.it>
2021-06-07 10:33:46 +0200

    fixes

--- orchestrator.py
+++ orchestrator.py
@@ -66,8 +66,9 @@ ces.MasterArbitrationUpdate()
 while True:
     packet = None
     print("Waiting for receive something")
-    packet = sniff(count = 1, prn=lambda x:x.sprintf("{IP:%IP.src%:%TCP.sport% > %IP.dst%TCP.dport%\n}{Raw:%Raw.load%\n}"))
+    packet = sniff(count = 1)
     
     if packet != None:
         print("Packet received!: " + packet[0][IP].src + "-->" + packet[0][IP].dst)
+        print(packet)
         checkPolicies(packet)
