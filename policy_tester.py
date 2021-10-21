a9ab33cf904b028025da3fe7ddcfc0e671930836
FrancescoBattagin <francesco.battagin@mail.polimi.it>
2021-10-21 12:14:54 +0200

    add functions that handle specific modifications to policyDB ([!] TOTEST)
    [!]TOADD: how to turn auth \wo ip into ip address

--- policy_tester.py
+++ policy_tester.py
@@ -23,7 +23,7 @@ def mod_detecter():
 
 
 #find out specific modifications per policy
-#[!] TOADD: function that manages specific modifications \w switch table
+#[!] TOADD: ip management when auth \wo ip
 def mod_manager():
     global policies_list
     tmp = policies_list
@@ -31,47 +31,102 @@ def mod_manager():
     
     found = False
 
-    for policy in policies_list:
-        print(policy)
+    for policy_tmp in tmp:
 
-        for policy_tmp in tmp:
+        for policy in policies_list:
+            print(policy)
+        
             if policy.get("serviceName") == policy_tmp.get("serviceName"):
                 found = True
                 print("[!] Service found: " + "--> " + policy.get("serviceName"))
             
                 if policy.get("ip") != policy_tmp.get("ip"):
                     print("[!] IP_MODIFICATIONS")
+                    print("[!] Editing policies IP...")
+                    editIPPolicies(policy_tmp.get("ip"), policy.get("ip"), policy.get("port"))
         
                 if policy.get("port") != policy_tmp.get("port"):
                     print("[!] PORT_MODIFICATIONS")
+                    print("[!] Editing policies Port...")
+                    editPortPolicies(policy_tmp.get("ip"), policy_tmp.get("port"))
 
                 if policy.get("protocol") != policy_tmp.get("protocol"):
                     print("[!] PROTOCOL_MODIFICATIONS")
-
+                    
+                #UE checks (add or del)
                 for ue in policy.get("allowed_users"):
-                    ue_mod = False
                     if ue not in policy_tmp.get("allowed_users"):
-                        ue_mod = True
-                        print("[!] UE_MODIFICATIONS")
-                        print("ue_mod:")
-                        print(ue_mod)
+                        print("[!] UE_MODIFICATIONS_ADD")
+                        addUE(ue_ip, policy.get("ip"), policy.get("port")) #[!] how to handle ue_ip \w auth different from ip?
+                for ue in policy_tmp.get("allowed_users"):
+                    if ue not in policy.get("allowed_users")
+                        print("[!] UE_MODIFICATIONS_DEL")
+                        delUE(ue_ip, policy.get("ip")) #[!] how to handle ue_ip \w auth different from ip?
 
+                #how to change it inside table_entry?
                 if policy.get("tee") != policy_tmp.get("tee"):
                     print("[!] TEE_MODIFICATIONS")
-
+                    
                 if policy.get("fs_encr") != policy_tmp.get("fs_encr"):
                     print("[!] FS_ENCR_MODIFICATIONS")
-
+                    
                 if policy.get("net_encr") != policy_tmp.get("net_encr"):
                     print("[!] NET_ENCR_MODIFICATIONS")
-
+                    
                 if policy.get("sec_boot") != policy_tmp.get("sec_boot"):
                     print("[!] SEC_BOOT_MODIFICATIONS")
-                
+                    
                 break
 
             if not found:
-                print("[!] Service not found")                              
+                print("[!] Service not found")   
+                print("[!] Deleting service policies...")
+                delPolicies(policy.get("ip"))
+
+#del policies when service not found
+def delPolicies(ip):
+    for te in table_entry["my_ingress.ipv4_exact"].read():
+        if te.match["hdr.ipv4.srcAddr"] == ip:
+            te.delete()
+
+#edit service ip 
+def editIPPolicies(old_ip, new_ip, port):
+    for te in table_entry["my_ingress.ipv4_exact"].read():
+        if te.match["hdr.ipv4.srcAddr"] == old_ip:
+            src_addr = te.match["hdr.ipv4.src_addr"]
+            te.delete()
+            te = sh.TableEntry('my_ingress.ipv4_exact')(action='my_ingress.ipv4_forward')
+            te.match["hdr.ipv4.srcAddr"] = src_addr
+            te.match["hdr.ipv4.dstAddr"] = new_ip
+            te.action["port"] = port
+            te.insert()
+
+#edit service port
+def editPortPolicies(ip, new_port):
+    for te in table_entry["my_ingress.ipv4_exact"].read():
+        if te.match["hdr.ipv4.srcAddr"] == ip:
+            src_addr = te.match["hdr.ipv4.src_addr"]
+            te.delete()
+            te = sh.TableEntry('my_ingress.ipv4_exact')(action='my_ingress.ipv4_forward')
+            te.match["hdr.ipv4.srcAddr"] = src_addr
+            te.match["hdr.ipv4.dstAddr"] = ip
+            te.action["port"] = new_port
+            te.insert() 
+
+#add a new policy (old service, new allowed user) 
+def addUE(ue_ip, service_ip, port):
+    te = sh.TableEntry('my_ingress.ipv4_exact')(action='my_ingress.ipv4_forward')
+    te.match["hdr.ipv4.srcAddr"] = ue_ip
+    te.match["hdr.ipv4.dstAddr"] = service_ip
+    te.action["port"] = port
+    te.insert()
+
+#delete a policy (old service, user not allowed anymore)
+def delUE(ue_ip, service_ip):
+    for te in table_entry["my_ingress.ipv4_exact"].read():
+        if te.match["hdr.ipv4.srcAddr"] == ue_ip and te.match["hdr.ipv4.dstAddr"] == service_ip:
+            te.delete()
+
 
 #getPolicies from policyDB     
 def getPolicies():
